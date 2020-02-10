68e8111aacb644b34d22ffceff40b695794cee37
Admin <Admin@DESKTOP-9MFTIHK>
2020-02-10 21:00:02 +0530

    commit

---
+++ calculator/src/calculator/calci.java
@@ -0,0 +1,21 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class calci {
+
+	public static void main(String[] args) {
+	  int num1,num2;
+	Scanner sc=new Scanner(system.in);
+	  System.out.println("enter 1st number:");
+	  num1=sc.nextInt();
+	  System.out.println("enter 2nd number");
+	  num2=sc.nextInt();
+	  System.out.println("addition="+(num1+num2));
+	  System.out.println("subtractioin="+(num1-num2));
+	  System.out.println("multiplication="+(num1*num2));
+	  System.out.println("division="+(num1/num2));
+	  
+	}
+
+}
