7cdec72749370655288bf6c84002198dc75d8692
Admin <Admin@DESKTOP-9MFTIHK>
2020-02-10 21:28:57 +0530

    first commit

---
+++ new project/src/helloPack/Calculator.java
@@ -0,0 +1,22 @@
+package helloPack;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		int num1,num2;
+		Scanner sc=new Scanner(system.in);
+		System.out.println("enter 1st number");
+		num1=sc.nextInt();
+		System.out.println("eneter 2nd number");
+		num2=sc.nextInt();
+		System.out.println("addition="+(num1+num2));
+		System.out.println("subtraction="+(num1-num2));
+		System.out.println("multiplication="+(num1*num2));
+		System.out.println("division="+(num1/num2));
+		
+		
+	}
+
+}
